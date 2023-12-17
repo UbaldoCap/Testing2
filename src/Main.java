16a8a7079232e8f13c2ec2f5ebe211cc64931087
Ubaldo <ubaldocapalbo@gmail.com>
2023-12-18 00:35:21 +0100

    end

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ import java.time.format.FormatStyle;
 
 public class Main {
     public static void main(String[] args) {
+        OffsetDateTime data = OffsetDateTime.parse("2002-03-01T13:00:00Z");
+        System.out.println(data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+        System.out.println(data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
+        System.out.println(data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)));
     }
     public String stampaFull(OffsetDateTime data1) {
         return data1.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
